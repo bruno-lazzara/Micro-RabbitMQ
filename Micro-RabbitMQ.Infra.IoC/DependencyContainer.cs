5f39d4e0f2d15f5b2e87dd703d2a7b7ea3670450
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-01-20 14:28:01 -0300

    Create project for Infra.IoC and register first service

---
+++ Micro-RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Micro_RabbitMQ.Domain.Core.Bus;
+using Micro_RabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Micro_RabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            #region Domain Bus
+
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            #endregion
+        }
+    }
+}
